207085d9615b32b39766792ffd79111b35126864
Kenil-29th <kenilp110043@gmail.com>
2026-08-17 20:56:08 +0530

    Add Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import ThreeAM from './ThreeAM.jsx'
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
